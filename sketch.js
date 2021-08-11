5e28209c333f8a257b71bb86e337b19099fb1f9b
Andrei Serban <andr3iser@gmail.com>
2021-08-11 12:31:33 +0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,25 +1,46 @@
-let bubble1;
-let bubble2;
+let bubbles = [];
+// let unicorn;
 
 function setup() {
   createCanvas(600, 400);
-  bubble1 = new Bubble(200, 200);
-  bubble2 = new Bubble(400, 200, 100);
+  for (let i = 0; i< 20; i++) {
+    let x = random(width);
+    let y = random(height);
+    let r = random(10, 50);
+    bubbles[i] = new Bubble(x, y, r);
+  }
+  // unicorn = new Bubble(400, 200, 10);
 }
+  
+ 
 
 function draw() {
   background(0);
-
-  if (bubble1.intersects(bubble2)) {
-    background(200, 0, 100);
+  
+ //   if (bubble1.intersects(bubble2)) {
+ //     background(200, 0, 100);
+ //   }
+ //   unicorn.x = mouseX;
+ //   unicorn.y = mouseY;
+ //   unicorn.show();
+ //   unicorn.move();
+  
+  for (let b of bubbles) {
+    b.show();
+    b.move();
+    let overlapping = false;
+    for (let other of bubbles) {
+      if (b !== other && b.intersects(other)) {
+        overlapping = true;  
+      }
+    }
+    if (overlapping) {
+      b.changeColor(255);
+    } else {
+      b.changeColor(0);
+    }
   }
-
-  bubble1.show();
-  bubble2.show();
-  bubble1.move();
-  //bubble2.move();
-  bubble2.x = mouseX;
-  bubble2.y = mouseY;
+  
 }
 
 class Bubble {
@@ -29,35 +50,35 @@ class Bubble {
     this.r = r;
     this.brightness = 0;
   }
-
+  
   intersects(other) {
     let d = dist(this.x, this.y, other.x, other.y);
-    return d < this.r + other.r;
+    return (d < this.r + other.r);
     // if (d < this.r + other.r) {
     //   return true;
     // } else {
     //   return false;
     // }
   }
-
+  
   changeColor(bright) {
     this.brightness = bright;
   }
-
-  contains(px, py) {
-    let d = dist(px, py, this.x, this.y);
+  
+  contains (px, py) {
+    let d = dist(this.x, this.y, other.x, other.y);
     if (d < this.r) {
       return true;
     } else {
       return false;
     }
   }
-
+  
   move() {
     this.x = this.x + random(-2, 2);
     this.y = this.y + random(-2, 2);
   }
-
+  
   show() {
     stroke(255);
     strokeWeight(4);
@@ -65,3 +86,4 @@ class Bubble {
     ellipse(this.x, this.y, this.r * 2);
   }
 }
+
